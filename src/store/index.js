2e392af8441df0bdccd962d93cb9021bf41267b4
Abhay <pym.part@gmail.com>
2023-04-03 17:33:43 +0530

    react form

---
+++ src/store/index.js
@@ -0,0 +1,24 @@
+import { createStore } from "redux"
+
+
+const reducerFn = (state={ 
+							counter: 0,
+							formData: ""
+					}, action) => {
+						if (action.type === "INCREMENT") {
+							return {counter: state.counter + 1}
+						} else if (action.type === "DECREMENT") {
+							return {counter: state.counter - 1}
+						} else if (action.type === "ADD") {
+							return {counter: state.counter + action.payload}
+						} else if (action.type === "ADD_CONTACT") {
+							return {formData: action.payload}
+						}
+					return state;
+
+};
+
+const store = createStore(reducerFn);
+
+export default store;
+
